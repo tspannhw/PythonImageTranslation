47562990cc3d9b8c2b4605f5541726fd72d0cb45
shingolavine <shingolavine@gmail.com>
2016-08-15 11:17:07 -0700

    cleaning up files again

--- clusterDefinition.py
+++
@@ -1,6 +0,0 @@
-from sailfish.config import MachineSpec
-
-nodes = [
-	MachineSpec('ssh=ubuntu@192.168.1.3', 'hosta', cuda_nvcc='/usr/local/cuda/bin/nvcc', gpus=[0]),
-	MachineSpec('ssh=ubuntu@localhost', 'hostb', cuda_nvcc='/usr/local/cuda/bin/nvcc', gpus=[0], block_size=256)
-]
--- variableSet.py
+++
@@ -1,5 +0,0 @@
-for line in open("walls.txt"):
-	wall = line
-
-wall = wall[:-3]
-print wall
